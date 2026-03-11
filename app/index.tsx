d18b5b89ebb695caa3803c95316f2b707ea1a44c
UP240259 <up240259@alumnos.upa.edu.mx>
2026-03-11 15:04:09 -0600

    Puse una barra para filtrar Pokemones

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import PokemoCard from "@/components/PokemoCard";
 import { useEffect, useState } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, TextInput } from "react-native";
 
 interface Pokemon {
   name: string;
@@ -9,41 +9,46 @@ interface Pokemon {
 
 export default function Index() {
   const [result, setResult] = useState<Pokemon[]>([]);
+  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
-    console.log("Entre en pantalla");
     getPokemons();
   }, []);
 
   const getPokemons = async () => {
     try {
       const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
-      const response = await fetch(URL, {
-        method: "GET",
-      });
+      const response = await fetch(URL, { method: "GET" });
 
       if (response.ok) {
         const data = await response.json();
         setResult(data.results);
+        setAllPokemons(data.results);
       } else {
-        console.log("Bard Request");
+        console.log("Bad Request");
       }
     } catch (error) {
-      console.log("Ocurrio un error");
+      console.log("Ocurrió un error");
+    }
+  };
+
+  const filterPokemon = (text: string) => {
+    if (text === "") {
+      setResult(allPokemons);
+    } else {
+      const arrayFiltered = allPokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(text.toLowerCase()),
+      );
+      setResult(arrayFiltered);
     }
   };
 
   return (
     <ScrollView>
-      {result.map((item) => {
-        return (
-          <PokemoCard
-            key={item.name}
-            name={item.name}
-            url={item.url}
-          ></PokemoCard>
-        );
-      })}
+      <TextInput onChangeText={filterPokemon} placeholder="Buscar pokémon..." />
+      {result.map((item) => (
+        <PokemoCard key={item.name} name={item.name} url={item.url} />
+      ))}
     </ScrollView>
   );
 }
